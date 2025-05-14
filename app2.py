cf4908b6bd8d829f6f609b9bc38eec7e28704c67
MathVini954 <mvgs@poli.br>
2025-05-14 15:11:26 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,23 +1,4 @@
-@st.cache_data
-def carregar_usuarios():
-    try:
-        return pd.read_excel("usuarios.xlsx")
-    except FileNotFoundError:
-        st.error("Arquivo 'usuarios.xlsx' não encontrado.")
-        st.stop()
-
-@st.cache_data
-def carregar_dados_efetivo():
-    try:
-        return pd.read_excel("efetivo.xlsx")
-    except FileNotFoundError:
-        st.error("Arquivo 'efetivo.xlsx' não encontrado.")
-        st.stop()
-
-@st.cache_data
-def carregar_dados_produtividade():
-    try:
-        return pd.read_excel("produtividade.xlsx")
-    except FileNotFoundError:
-        st.error("Arquivo 'produtividade.xlsx' não encontrado.")
-        st.stop()
+@st.cache
+def carregar_dados(arquivo):
+    df = pd.read_excel(arquivo, engine="openpyxl")
+    ...

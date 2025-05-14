97d5b0d71e6340b727231da51a5a6f28166b2ae2
MathVini954 <mvgs@poli.br>
2025-05-14 17:18:55 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from datetime import datetime
 
 # --- Configuração da página ---
 st.set_page_config(page_title="Dashboard Integrado", layout="wide")

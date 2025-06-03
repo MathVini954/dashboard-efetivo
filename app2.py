4d65b81c3f24420880a52c8426f5690c36562cf9
MathVini954 <mvgs@poli.br>
2025-06-03 17:36:55 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -50,7 +50,7 @@ def definir_colunas_ganhos_descontos():
         'Atrasos', 'Faltas em Dias', 'DESCONTO DE ALIMENTAÇÃO', 'MENSALIDADE SINDICAL',
         'Vale Transporte', 'Assistencia Medica', 'Coparticipacao Dependente', 'Coparticipacao Titular', 'Desconto Empréstimo',
         'Diferenca Plano De Saude', 'Desconto Ótica', 'Plano Odontologico',
-        'Plano Odontologico Dependente', 'Pensão Alimentícia  Salário Mínimo',
+        'Plano Odontologico Dependente', 'Pensão Alimentícia Salário Mínimo',
         'Assitência Médica Dependente', 'Dsr sobre falta', 'INSS Folha', 'IRRF Folha', 'Pensão Alimentícia'
     ]
     
